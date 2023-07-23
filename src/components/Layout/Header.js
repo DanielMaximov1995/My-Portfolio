0ac362a269862c02b9204bb6e0ed299be7a2de81
Daniel Maximov <danielmaximov2@outlook.com>
2023-07-23 23:45:21 +0300

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,7 +5,7 @@ import Socials from "@/components/Socials";
 const Header = () => {
   return <header className='absolute z-30 w-full flex items-center xl:px-0 xl:h-[90px]'>
     <div className='container mx-auto'>
-      <div className='flex flex-row justify-around items-center gap-x-8 gap-y-8 py-8'>
+      <div className='flex flex-row justify-between items-center gap-x-8 gap-y-8 py-8'>
         <Link href='/'>
           <Image src='/logo.svg' alt='Daniel Maximov Full stack Developer' width={220} height={48} priority={true}/>
         </Link>

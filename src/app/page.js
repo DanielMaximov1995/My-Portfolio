1167bc33e5ff5f115f9fbf778f066536a0ebbdc4
Daniel Maximov <139768935+DanielMaximov1995@users.noreply.github.com>
2023-07-26 17:57:24 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Home from "@/components/Home Page/Home";
 import {getPage} from "@/sanity/sanity-utils";
 
+export const revalidate = 60
+
 const Page = async () => {
     let page = await getPage('home')
     return <Home content={page}/>

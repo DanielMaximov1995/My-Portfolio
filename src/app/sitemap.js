2505bbeae2934f5451e16f6324b7ecb0c2b79699
Daniel Maximov <danielmaximov2@outlook.com>
2023-07-31 18:27:02 +0300

    seo update

---
+++ src/app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+    const baseUrl = 'https://daniel-maximov.vercel.app'
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/projects`,
+            lastModified: new Date(),
+        },
+    ]
+}
